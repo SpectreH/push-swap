df9d3947d8a0669a637c524954b8c0dfeff243dd
SpectreH <denni.karin2803@gmail.com>
2021-11-03 23:03:43 +0200

    update numbers generator, exclude repeats

--- test.sh
+++ test.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 numbers=""
+numbersArr=()
 for i in {1..5}; do
-  numbers+="$((RANDOM % 100))" 
+  declare -i numberToAppend="$((RANDOM % 100))"
+  repeats=no
+  while [[ "$repeats" = "no" ]] 
+  do
+    repeats=yes 
+    for y in "${numbersArr[@]}"; do
+      if [ "$y" = "$numberToAppend" ]; then
+      repeats=no 
+      numberToAppend=$(($y + 1))
+      fi
+    done
+  done
+  numbers+="$numberToAppend"
+  numbersArr+=("$numberToAppend")
   if [ "$i" != 5 ]; then
   numbers+=" " 
   fi
